336f3b841ed3e8336ad768ed31162ed63da09117
jayesh <choudhari.jayesh@iitgn.ac.in>
2021-06-03 17:49:18 +0100

    change in command line arguments and respective changes...

--- mainCount.cpp
+++ mainCount.cpp
@@ -3,7 +3,6 @@
 #include "include/count3Graphlets.h"
 #include "include/count4Graphlets.h"
 #include "include/utilities.h"
-// #include <bits/stdc++.h>
 #include <iomanip>
 #include <ctime>
 #include <chrono>
@@ -12,12 +11,24 @@
 
 int main(int argc, char *argv[])
 {
-    string inputFileName = argv[1];
+    if (argc != 4) 
+	{
+        std::cerr << "ERROR -- Requires 3 parameters: 1) Input Graph filename; 2) CodeWord for Graphlet To Count; and 3) Output filename;\n";
+        std::cout << "Codeword for Graphlet to Count -- :\n" 
+                    << "1) g32: Triangle (3-Clique)\n" 
+                    << "2) g43: 4-Cycle\n"
+                    << "3) g45: 4-Chord-Cycle\n" 
+                    << "4) g46: 4-Clique\n" 
+                    << "Run code to count 3-Clique or Triangles: ./main-organized-make InputGraphFile g32 OutputFile";  
+        exit(1);
+    }
 
-    cout << "initializing graph...\n";
+    string inputFileName = argv[1];
+    string whatCount = argv[2];
+    string outFileName = argv[3];
+
+    cout << "Initializing graph... Populating Edges in Memory...\n";
 	Graph G(inputFileName);				// constructor
-	// Graph G(numV, e);
-	// e.clear();
 
 	Count numVertices = G.getNumVertices();
 	Count numEdges = G.getNumEdges();
@@ -26,48 +37,41 @@ int main(int argc, char *argv[])
     cout << "n =  " << numVertices << endl;
     cout << "E =  " << numEdges << endl;
 
-    // int maxK;
-	int numRandomWalks = 10;
+	int numRandomWalks = 100;
 	
-    int lStep = stoi(argv[2]);      /// not using this argument currently
-
-    string outFileName = argv[3];
-	// lStep = numEdges / 20;
-
     ofstream outFile;
     outFile.open(outFileName, ofstream::out);
 
-    // for(int i = 0; i < 1; ++i)
-    // {
+    // if(whatCount.find("g3") != std::string::npos)
+    //     rwCount3Graphlets C3;
+    // else if(whatCount.find("g4") != std::string::npos)
+    //     rwCount4Graphlets C4;
+
     vector<OrderedEdge> rWEdges;
     vector<OrderedEdge>::iterator rWEdgesIt;
 
     double avgVal = 0;
     cout.precision(20);
+
     std::chrono::steady_clock::time_point beginClock;
     std::chrono::steady_clock::time_point endClock;
     long long int perIterationTime = 0, totalTimePerEdgePerc = 0;
 
-    // cout << "No floating point exceptions here\n";
-
-    srand(10);
+    srand(10000);
     // int randStartPoint = rand() % numVertices;
     cout << "before the start point -- \n";
     VertexIdx randStartPoint = next() % numVertices;
     cout << "Got start point -- "<< randStartPoint << "\n";
 
-    rwCount3Graphlets C3;
-    // rwCount4Graphlets C4;
-
     // vector<int> percEdges = {1, 5, 7, 10};
-    // vector<double> percEdges = {0.1, 0.3, 0.5, 0.7};
+    vector<double> percEdges = {0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0};
     // vector<double> percEdges = {1.5, 2.0, 2.5, 3.0};
-    vector<double> percEdges = {3.0};
+    // vector<double> percEdges = {3.0};
 
     cout << "Going for random walks....\n";
     for(unsigned int j = 0; j < percEdges.size(); j++)
     {
-        lStep = percEdges[j] * (numEdges / 100);
+        int lStep = percEdges[j] * (numEdges / 100);
         outFile << "% Edges = " << percEdges[j] << endl;
         
         vector<double> allRunEsts;
@@ -78,14 +82,36 @@ int main(int argc, char *argv[])
             rWEdges = G.getAllEdgesFromRStepRandomWalk(lStep, randStartPoint);
             cout << k << "th Random Walk -- Got random walk edges.... -- " << rWEdges.size() << "---" << percEdges[j] << "\n";
             
-            double kGraphletCount =  C3.countTriangleGraphlet(G, rWEdges);	// passing all seg_2's
-            // double kGraphletCount =  C4.count4CliqueGraphlet(G, rWEdges);	// passing all seg_2's
-            // double kGraphletCount =  C4.count4ChordCycle(G, rWEdges);	// passing all seg_2's
+            double kGraphletCount = 0;
+
+            if(whatCount.compare("g32") == 0)
+            {
+                rwCount3Graphlets C3;
+                kGraphletCount =  C3.countTriangleGraphlet(G, rWEdges);	// passing all seg_2's
+            }
+            else if(whatCount.compare("g43") == 0)
+            {
+                rwCount4Graphlets C4;
+                // kGraphletCount =  C4.count4Cycle(G, rWEdges);	// passing all seg_2's
+                std::cout << "To be added...\n";
+            }
+            else if(whatCount.compare("g45") == 0)
+            {
+                rwCount4Graphlets C4;
+                kGraphletCount =  C4.count4ChordCycle(G, rWEdges);	// passing all seg_2's
+            }
+            else if(whatCount.compare("g46") == 0)
+            {
+                rwCount4Graphlets C4;
+                kGraphletCount =  C4.count4CliqueGraphlet(G, rWEdges);	// passing all seg_2's
+            }
             endClock = chrono::steady_clock::now();
+
             perIterationTime = chrono::duration_cast<std::chrono::microseconds> (endClock - beginClock).count();
             totalTimePerEdgePerc += perIterationTime;
-            cout << k << "-th Estimate = " << kGraphletCount << endl;
-            outFile << setprecision(20) << kGraphletCount << " " << setprecision(20) <<  perIterationTime << endl;
+            // cout << k << "-th Estimate = " << kGraphletCount << endl;
+            cout << k << " " << percEdges[j] << " " << setprecision(20) << kGraphletCount << " " << setprecision(20) <<  perIterationTime << endl;
+            outFile << k << " " << percEdges[j] << " " << setprecision(20) << kGraphletCount << " " << setprecision(20) <<  perIterationTime << endl;
             
             allRunEsts.push_back(kGraphletCount);
             avgVal = avgVal + (kGraphletCount - avgVal)/(k+1);
@@ -94,7 +120,7 @@ int main(int argc, char *argv[])
             cout << "Time per iteration = " << perIterationTime << endl;
         }
     }
-    // }
 
+    outFile.close();
     return 0;
 }
