5a308cfd2dbf9057fe835811fa6f29e220a53fa2
Jayesh Choudhari <choudhari.jayesh@iitgn.ac.in>
2022-02-07 16:56:31 +0000

    Change in the last argument for SRW
    Change in the last argument for SRW

--- include/count5Graphlets.h
+++ include/count5Graphlets.h
@@ -8,7 +8,7 @@ class rwCount5Graphlets
         double count5CliqueBut1Edge(Graph &G, vector<OrderedEdge> rwEdges, int l3Perc, int l4Perc, int l5Perc);
         double count5CliqueBut2Edges(Graph &G, vector<OrderedEdge> rwEdges, int l3Perc, int l4Perc, int l5Perc);
 
-        double SRWCount5CliqueGraphlet(Graph &G, int numSteps, Count numEdgesInG2);
+        double SRWCount5CliqueGraphlet(Graph &G, int numSteps, double numEdgesInG2);
         vector<VertexIdx> sampleUARNeighboringEdge(Graph &G, VertexIdx u, VertexIdx v);
         vector<vector<VertexIdx>> getX3Edges(Graph &G, VertexIdx startNode);
         vector<Count> getDegreeOfEdges(Graph &G, vector<vector<VertexIdx>> edgeList);
